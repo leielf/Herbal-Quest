3f8399e52ac440fbce8c9af2ff2c7c37b1db0098
Leila Babayeva <babaylei@fel.cvut.cz>
2024-05-04 17:22:26 +0200

    adding thread

--- src/main/java/com/example/elixirapp/ElixirApplication.java
+++ src/main/java/com/example/elixirapp/ElixirApplication.java
@@ -1,108 +1,53 @@
 package com.example.elixirapp;
 
-import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.beans.binding.Bindings;
-import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
-import javafx.scene.layout.*;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
-public class ElixirApplication extends Application {
-    private Player player;
+public class ElixirApplication extends Application implements Runnable{
 
     private static final Logger logger = Logger.getLogger(ElixirApplication.class.getName());
-
-    private int starting_point = 0;
+    private Thread gameThread;
+    LevelController levelController = new LevelController();
     @Override
     public void start(Stage stage) throws IOException {
-        LevelManager lvlManager = new LevelManager();
-        lvlManager.startStage(stage, "/Users/leielf/Downloads/second_json.json");
-//        Pane pane = new Pane();
-//        pane.setMinSize(6335, 708);
-//        pane.setPrefSize(6335, 708);
-//        pane.setMaxSize(6335, 708);
-//
-//        Image img = new Image("/background_long.png");
-//        ImageView backgroundView = new ImageView(img);
-//        pane.getChildren().add(backgroundView);
-//
-//        Scene scene = new Scene(new BorderPane(pane), 1268, 708);
-//        javafx.scene.shape.Rectangle clip = new Rectangle();
-//        clip.widthProperty().bind(scene.widthProperty());
-//        clip.heightProperty().bind(scene.heightProperty());
-//
-//        LevelLoader lvlLoader = new LevelLoader();
-//        lvlLoader.addToLevel(pane, "/Users/leielf/Downloads/second_json.json");
-
-//        Image playerImg = new Image("/masculine-user.png");
-//        player = new Player(playerImg, 1268/2-80, 500, 10);
-//        pane.getChildren().add(player.getImg());
-//
-//        clip.xProperty().bind(Bindings.createDoubleBinding(
-//                () -> clampRange(player.getImg().getX() - scene.getWidth() / 2 +80, starting_point, pane.getWidth() - scene.getWidth()),
-//                player.getImg().xProperty(), scene.widthProperty(), pane.widthProperty())
-//        );
-//        pane.setClip(clip);
-//        pane.translateXProperty().bind(clip.xProperty().multiply(-1));
-//
-//        scene.setOnKeyPressed(e -> processKey(e.getCode(), true));
-//        scene.setOnKeyReleased(e -> processKey(e.getCode(), false));
-//
-//        AnimationTimer timer = new AnimationTimer() {
-//            @Override
-//            public void handle(long now) {
-//                if (player.isRight()) {
-//                    if ((player.getX() - clip.getX()) == scene.getWidth()/2-80){
-//                        starting_point += player.getVelX();
-//                    }
-//                }
-//                player.moveLeftRight(starting_point, pane.getWidth() - player.getImg().getFitWidth());
-//                player.jump();
-////                logger.log(Level.INFO,"player's X: " + player.getX()+", starting point = " + starting_point);
-//            }
-//        };
-//        stage.setScene(scene);
-//        stage.show();
-//        timer.start();
+        levelController.createLevel(stage, "/Users/leielf/Downloads/second_json.json");
+        startGameThread();
     }
 
-
-
-    private double clampRange(double value, double min, double max) {
-        if (value < min) return min ;
-        if (value > max) return max ;
-        return value ;
+    public void startGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start();
     }
-
-    private void processKey(KeyCode code, boolean on) {
-        switch (code) {
-            case LEFT:
-                player.setLeft(on) ;
-                logger.log(Level.INFO, "<- was clicked");
-                break ;
-            case RIGHT:
-                player.setRight(on);
-                logger.log(Level.INFO, "-> was clicked");
-                break ;
-            case UP:
-                player.setJumping(on);
-                player.setFalling(!on);
-                break ;
-            default:
-                break ;
+    @Override
+    public void run() {
+        double updateInterval = 1000000000/60;
+        double nextUpdateTime = System.nanoTime() + updateInterval;
+        while (gameThread != null){
+            if(levelController.isPlayerDead()){
+                gameThread = null;
+            }
+            levelController.gameUpdate();
+            try {
+                double remainingTime = nextUpdateTime - System.nanoTime();
+                remainingTime /= 1000000;
+                if (remainingTime < 0){
+                    remainingTime = 0;
+                }
+                Thread.sleep((long)remainingTime);
+                nextUpdateTime += updateInterval;
+            }catch (InterruptedException e){
+                logger.log(Level.INFO, "error in thread");
+            }
         }
     }
+
 
     public static void main(String[] args) {
         launch();
     }
+
 }
