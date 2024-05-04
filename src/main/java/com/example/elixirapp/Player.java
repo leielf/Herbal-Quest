dab0e28d28cd3748eb2ab9a46e58bab91477e570
Leila Babayeva <babaylei@fel.cvut.cz>
2024-05-04 17:17:23 +0200

    adding imgPath, min and max for moving

--- src/main/java/com/example/elixirapp/Player.java
+++ src/main/java/com/example/elixirapp/Player.java
@@ -1,14 +1,8 @@
 package com.example.elixirapp;
 
-import javafx.scene.image.Image;
-import javafx.scene.layout.Pane;
-
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Player extends GameObject{
-    private static final Logger logger = Logger.getLogger(Player.class.getName());
     private int coins = 0;
     private boolean isDead;
     private int velX;
@@ -21,16 +15,17 @@ public class Player extends GameObject{
     private boolean falling = false;
     private double velY;
     private double gravityAcc;
-    public Player(Image img, double x, double y, int velX) {
-        super(img, x, y);
+
+    private double min, max;
+    public Player(double x, double y, int velX) {
+        super(x, y);
         this.velX = velX;
-        setVelY(15);
-        getImg().setFitWidth(80);
-        getImg().setFitHeight(100);
         setGravityAcc(0.4);
         jumping = false;
-        falling = true;
+        falling = false;
+        this.setImagePath("/masculine-user.png");
     }
+
 
     public boolean isFalling() {
         return falling;
@@ -56,7 +51,8 @@ public class Player extends GameObject{
         this.gravityAcc = gravityAcc;
     }
 
-    public void moveLeftRight(int min, double max) {
+    public void move() {
+        velX = 7;
         if (right) {
             if (getX() < max){
                 setX(getX()+ velX);
@@ -70,25 +66,6 @@ public class Player extends GameObject{
             }else{
                 setX(min);
             }
-        }
-    }
-
-    public void jump() {
-        if(jumping && velY <= 0){
-            jumping = false;
-            falling = true;
-        }
-        else if(jumping){
-            velY = velY - gravityAcc;
-            setY(getY() - velY);
-        }
-        if(falling){
-            setY(getY() + velY);
-            velY = velY + gravityAcc;
-        }
-        if(falling && getY() > 500){
-            jumping = false;
-            falling = false;
         }
     }
 
@@ -124,10 +101,6 @@ public class Player extends GameObject{
         this.jumping = jumping;
     }
 
-    public void collideWith(Pane pane){
-
-    }
-
 
     public void collectCoin(Coin coin){
         this.coins+=coin.getValue();
@@ -137,7 +110,49 @@ public class Player extends GameObject{
         return coins;
     }
 
+    public void setCoins(int coins) {
+        this.coins = coins;
+    }
+
     public void collectHerb(Herb herb){
         this.herbs.add(herb);
+    }
+
+    public void jump() {
+        if(!isJumping() && !isFalling()){
+            setJumping(true);
+            setVelY(12);
+        }
+    }
+    @Override
+    public void updateLocation() {
+        if(jumping && velY <= 0){
+            jumping = false;
+            falling = true;
+        }
+        else if(jumping){
+            velY = velY - gravityAcc;
+            setY(getY()  - velY);
+        }
+        if(falling){
+            setY(getY()  + velY);
+            velY = velY + gravityAcc;
+        }
+    }
+
+    public void setDead(boolean dead) {
+        isDead = dead;
+    }
+
+    public boolean isDead() {
+        return isDead;
+    }
+
+    public void setMin(double min) {
+        this.min = min;
+    }
+
+    public void setMax(double max) {
+        this.max = max;
     }
 }
