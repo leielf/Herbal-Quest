a93d7c235352b2c556872c210f8f60967a4478bd
Leila Babayeva <babaylei@fel.cvut.cz>
2024-05-04 17:15:07 +0200

    function for moving from side to side

--- src/main/java/com/example/elixirapp/Thief.java
+++ src/main/java/com/example/elixirapp/Thief.java
@@ -3,19 +3,48 @@ package com.example.elixirapp;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Thief extends GameObject{
+    private static final int RANGE = 200;
+    private int max, min;
+    private int speed = 3;
+    private int direction = 1;
 
-    private boolean steal;
-    private int speed;
-
-    public Thief(Image img, double x, double y) {
-        super(img, x, y);
+    public Thief(double x, double y) {
+        super(x, y);
+        max = (int)x+RANGE;
+        min = (int)x;
+        this.setImagePath("/thief.png");
     }
 
+
+    public void updateLocation(boolean intersects, double blockY, int i) {
+        if(intersects) {
+            setFalling(false);
+            setY(blockY - getHeight());
+        }
+        if(getY() > 600 - getHeight()){
+            setFalling(false);
+            setY(blockY - getHeight());
+        }
+        if(isFalling() && !intersects){
+            setY(getY()  + getVelY());
+        }
+    }
 
     public void moveLeftRight(){
-
+        if(getX() >= min && getX() <= max) {
+            setX(getX() + speed*direction);
+        }else{
+            direction = direction*(-1);
+            if(getX() >= max) {
+                setX(max);
+            }else{
+                setX(min);
+            }
+        }
     }
-
 
 }
