8f8bf7f4747ee99c453dea21ffadf25fa5777462
Leila Babayeva <babaylei@fel.cvut.cz>
2024-05-04 17:11:27 +0200

    setting path to block image

--- src/main/java/com/example/elixirapp/Block.java
+++ src/main/java/com/example/elixirapp/Block.java
@@ -1,11 +1,9 @@
 package com.example.elixirapp;
 
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-
 public class Block extends GameObject{
 
-    public Block(Image img, double x, double y) {
-        super(img, x, y);
+    public Block(double x, double y) {
+        super(x, y);
+        this.setImagePath("/block.png");
     }
 }
