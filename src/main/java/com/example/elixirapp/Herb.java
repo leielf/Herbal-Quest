fbe02e5b9dabaaa72ababc1ebae71ffec261c7d3
Leila Babayeva <babaylei@fel.cvut.cz>
2024-05-14 10:46:45 +0200

    adding constructor, getters and setters

--- src/main/java/com/example/elixirapp/Herb.java
+++ src/main/java/com/example/elixirapp/Herb.java
@@ -1,7 +1,29 @@
 package com.example.elixirapp;
 
-public class Herb{
-    private int cost;
+public class Herb extends GameObject{
     private String name;
     private boolean isCollected;
+
+    public Herb(String name, double x, double y) {
+        super(x, y);
+        this.name = name;
+        isCollected = false;
+        this.setImagePath("/red_circle.png");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isCollected() {
+        return isCollected;
+    }
+
+    public void setCollected(boolean collected) {
+        isCollected = collected;
+    }
 }
