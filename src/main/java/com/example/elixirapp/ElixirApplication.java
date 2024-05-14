1043ce649b9e349be9b981faac1bfc153f53e109
Leila Babayeva <babaylei@fel.cvut.cz>
2024-05-14 11:23:52 +0200

    transfering everthing to GameEngine

--- src/main/java/com/example/elixirapp/ElixirApplication.java
+++ src/main/java/com/example/elixirapp/ElixirApplication.java
@@ -3,48 +3,15 @@ package com.example.elixirapp;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 
-public class ElixirApplication extends Application implements Runnable{
+public class ElixirApplication extends Application{
 
-    private static final Logger logger = Logger.getLogger(ElixirApplication.class.getName());
-    private Thread gameThread;
-    LevelController levelController = new LevelController();
     @Override
     public void start(Stage stage) throws IOException {
-        levelController.createLevel(stage, "/Users/leielf/Downloads/second_json.json");
-        startGameThread();
+        GameEngine gameEngine = new GameEngine(stage);
+        gameEngine.start("/Users/leielf/Downloads/second_json.json");
     }
-
-    public void startGameThread(){
-        gameThread = new Thread(this);
-        gameThread.start();
-    }
-    @Override
-    public void run() {
-        double updateInterval = 1000000000/60;
-        double nextUpdateTime = System.nanoTime() + updateInterval;
-        while (gameThread != null){
-            if(levelController.isPlayerDead()){
-                gameThread = null;
-            }
-            levelController.gameUpdate();
-            try {
-                double remainingTime = nextUpdateTime - System.nanoTime();
-                remainingTime /= 1000000;
-                if (remainingTime < 0){
-                    remainingTime = 0;
-                }
-                Thread.sleep((long)remainingTime);
-                nextUpdateTime += updateInterval;
-            }catch (InterruptedException e){
-                logger.log(Level.INFO, "error in thread");
-            }
-        }
-    }
-
 
     public static void main(String[] args) {
         launch();
